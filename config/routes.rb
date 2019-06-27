44cf954e835d5a57f0f58c72d8780e881800f8d3
j2FunOnly <j2funonly@ya.ru>
2019-06-27 12:23:10 +0300

    Add Users controller with new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
   # get '/help', to: 'static_pages#help'
   match '/help', to: 'static_pages#help', via: :get
   get '/about', to: 'static_pages#about', as: :about
   get '/contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
 end
