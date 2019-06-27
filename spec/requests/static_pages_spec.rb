36cb69ef60b78cce2b522ff4210286ccc995477f
j2FunOnly <j2funonly@ya.ru>
2019-06-27 08:14:12 +0300

    Lecture 3 Tasks 1, 2 complete

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'StaticPages' do
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe 'Home page' do
     before(:each) { visit '/' }
 
@@ -9,7 +11,7 @@ describe 'StaticPages' do
     end
 
     it 'have "Home" title' do
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -21,7 +23,7 @@ describe 'StaticPages' do
     end
 
     it 'have "Help" title' do
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -33,7 +35,19 @@ describe 'StaticPages' do
     end
 
     it 'have "About Us" title' do
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe 'Contact page' do
+    before(:each) { visit '/contact' }
+
+    it 'have the content "Contact Us"' do
+      expect(page).to have_content('Contact Us')
+    end
+
+    it 'have "Contact Us" title' do
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
