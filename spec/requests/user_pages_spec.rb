aa058c2a8987e71945e1cc11a1de7e9b46508c53
j2FunOnly <j2funonly@ya.ru>
2019-06-28 09:47:10 +0300

    Lecture 7 Tasks Complete

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,16 +14,33 @@ describe 'User pages' do
       it 'should not create user' do
         expect { click_button submit}.not_to change(User, :count)
       end
+
+      describe 'after submission' do
+        before { click_button submit }
+
+        it { should have_title('Sign Up') }
+        it { should have_content('error') }
+      end
     end
 
     describe 'with valid info' do
-      it 'should create user' do
+      before do
         fill_in 'Name', with: 'Test User'
         fill_in 'Email', with: 'test@example.com'
         fill_in 'Password', with: 'asdfqwerty'
         fill_in 'Password confirmation', with: 'asdfqwerty'
+      end
 
+      it 'should create user' do
         expect { click_button submit }.to change(User, :count)
+      end
+
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by_email('test@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
